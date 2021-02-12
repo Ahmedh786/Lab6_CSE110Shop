77b90e84a9550c8d7b05acf97fe77831f25376e2
Ahmedh786 <ahmedhussaini1999@gmail.com>
2021-02-12 11:28:47 -0800

    parts 1 and 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,18 @@
 // Script.js
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
   // TODO
+  if (localStorage.getItem("products") == null) {
+    var objArray = await fetch('https://fakestoreapi.com/products');
+    let text = await objArray.text();
+    //console.log(text);
+    localStorage.setItem("product", text);
+    if (localStorage.getItem("product") === null) {
+    //  console.log("OK");
+    }
+  }
+  else {
+    console.log("ALREADY HERE");
+  }
+
 });
